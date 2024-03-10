d55017b934da3891096c27a3a3593fbbb65c41b5
Muhammad Haidar Matin <114727612+darmtiin17@users.noreply.github.com>
2024-03-10 08:29:26 +0700

    Create App.java

---
+++ App.java
@@ -0,0 +1,20 @@
+public class App{ 
+    public static void main(String[] args) throws Exception {
+        RightTriangle ABC = new RightTriangle("ABC", 3.0, 4.0);
+        Triangle BCD = new RightTriangle("BCD", 3.0, 4.0);
+        Triangle CDE = new Triangle("CDE", 3.0, 4.0, 5.0);
+        Triangle DEF = new SameSideTriangle("BCD", 3.0, 4.0);
+        Triangle EFG = new SameFootTriangle("CDE", 3.0, 4.0, 5.0);
+
+
+        
+    
+        ABC.getInfo();
+        BCD.getInfo();
+        CDE.getInfo();
+        DEF.getInfo();
+        EFG.getInfo();
+        
+    }
+    
+}
