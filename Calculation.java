08365376e15465b971619a5b54e86355f939636d
Muhammad Haidar Matin <114727612+darmtiin17@users.noreply.github.com>
2024-03-10 08:32:03 +0700

    Create Calculation.java

---
+++ Calculation.java
@@ -0,0 +1,24 @@
+// Dynamic polymorphism using Overriding method, Run-time Binding / late Binding
+
+public class Calculation {
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int add (Double c, int d){
+        int x = (int) c.doubleValue();
+        return x + d;
+    }
+
+    public int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+    public double add(double a, double b){
+        return a + b;
+    }
+
+    public double add(double a, double b, double c){
+        return a + b + c;
+    }
+}
